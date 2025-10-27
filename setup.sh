62dd356f4a1d4e3ac471d65bdad2914459fb5ea7
Ömer Karaca <omerkaracca@gmail.com>
2025-10-27 19:52:54 +0300

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+create_symlink() {
+    local src="$1"
+    local dest="$2"
+
+    if [[ "$src" != /* ]]; then
+        src="$(pwd)/$src"
+    fi
+
+    if [ -L "$dest" ]; then
+        if [ "$(readlink "$dest")" = "$src" ]; then
+            echo "Symlink $dest already points to $src - skipping"
+            return
+        fi
+        echo "Backing up existing $dest"
+        mv "$dest" "${dest}.backup"
+    elif [ -e "$dest" ]; then
+        echo "Backing up existing $dest"
+        mv "$dest" "${dest}.backup"
+    fi
+
+    echo "Creating symlink for $src"
+    ln -s "$src" "$dest"
+}
+
+create_symlink "alacritty/theme.toml" "$HOME/.alacritty-theme.toml"
+create_symlink "alacritty/alacritty.toml" "$HOME/.alacritty.toml"
