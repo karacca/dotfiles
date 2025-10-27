6896086fae25f9bbd9c8dab5f217e8da8668d700
Ömer Karaca <omerkaracca@gmail.com>
2025-10-27 20:08:09 +0300

    Add macOS settings

---
+++ macos/setup.sh
@@ -0,0 +1,4 @@
+defaults write com.apple.dock static-only -bool true
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -int 0
+killall Dock
